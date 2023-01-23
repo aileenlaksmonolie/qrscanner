854d116332473a2a5b584809edcf36cbf1dec5d7
aileenlaksmonolie <47419970+aileenlaksmonolie@users.noreply.github.com>
2023-01-23 12:01:57 +0800

    first commit2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Qrscan from './Qrscan';  // QR code scanner
+
+function App() {
+  return (
+    <div>
+      <Qrscan />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Qrscan.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import QrReader from "react-web-qr-reader";
+import axios from 'axios';
+import styles from './Qrscan.module.css';
+
+const Qrscan = () => {
+
+	const [result, setResult] = useState("");
+	const [loading, setLoading] = useState('');
+	const [APIResult, setAPIResult] = useState('');
+
+	const handleError = (err) => {
+		console.err(err)
+	}
+
+	const handleScan = (result) => {
+		if(result!==""){
+            setLoading("Loading....");
+            setResult(result.data);
+            axios.post(`https://icnmusical.com/api/scanner/check_in`, { "encrypted_ticket_item_id":result.data })
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+                if(res.data.result===false){
+                    setLoading("done!");
+                    setAPIResult(res.data.result.toString() + ", "+ res.data.error_message.toString());
+                }else{
+                    setLoading("done!");
+                    setAPIResult(res.data.result.toString() + ", checked in");
+                }
+            })
+            setResult("");
+		}
+	}
+
+	const previewStyle = {
+		height: 240,
+		width: 320,
+	}
+
+	return (
+		<div className={styles.container}>
+			<QrReader
+			delay={500}
+			style={previewStyle}
+			onError={handleError}
+			onScan={handleScan}
+			/>
+			<div className={styles.result}>scanning status: {loading}</div>	
+			<div className={styles.result}>checkin status: {APIResult}</div>	
+		</div>
+	);
+}
+
+export default Qrscan;
